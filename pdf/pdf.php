4e29d8347b95c61a7a46c11aa88010283d6e188c
takacsdaniel477 <takacsdaniel477@gmail.com>
2024-11-22 12:04:13 +0100

    pdf update

--- pdf/pdf.php
+++ pdf/pdf.php
@@ -1,32 +1,46 @@
+<!DOCTYPE HTML>
+<!--
+	Editorial by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>PDF létrehozás</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="../assets/css/main.css" />
+	</head>
+	<body class="is-preload">
 
-<!DOCTYPE html>
-<html lang="hu">
-<head>
-    <meta charset="UTF-8">
-    <title>PDF - Találmányok adatainak lekérése</title>
-</head>
-<body>
-    <div class="form-container">
-        <h2>Találmányok adatainak lekérése</h2>
-        <form method="POST" action="create.php" target="_blank">
-		
-            <div class="form-group">
-                <label for="nev">Feltaláló neve:</label>
-                <input type="text" id="nev" name="nev" value="">
-            </div>
-			<br>
-            <div class="form-group">
-                <label for="talnev">Találmány neve:</label>
-                <input type="text" id="talnev" name="talnev" value="">
-            </div>
-			<br>
-            <div class="form-group">
-                <label for="szuletes">Születési év nagyobb, mint:</label>
-                <input type="text" id="szuletes" name="szuletes" value="">
-            </div>
-            <br>
-            <button type="submit">PDF létrehozás</button>
-        </form>
-    </div>
-</body>
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Main -->
+					<div id="main">
+						<div class="inner">
+							<h2>Találmányok adatainak lekérése</h2>
+							<form method="POST" action="create.php" target="_blank">
+							
+								<div class="form-group">
+									<label for="nev">Feltaláló neve:</label>
+									<input type="text" id="nev" name="nev" value="">
+								</div>
+								<br>
+								<div class="form-group">
+									<label for="talnev">Találmány neve:</label>
+									<input type="text" id="talnev" name="talnev" value="">
+								</div>
+								<br>
+								<div class="form-group">
+									<label for="szuletes">Születési év nagyobb, mint:</label>
+									<input type="text" id="szuletes" name="szuletes" value="">
+								</div>
+								<br>
+								<button type="submit">PDF létrehozás</button>
+							</form>
+						</div>
+					</div>
+			</div>
+	</body>
 </html>
